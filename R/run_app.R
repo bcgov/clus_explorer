6756d021ddacceacf4ee00293f4944a5eff99266
Sasha Bogdanovic <sasha@rubyind.com>
2021-11-17 09:43:13 -0800

    Initialize renv and config

--- R/run_app.R
+++ R/run_app.R
@@ -1,28 +1,28 @@
 #' Run the Shiny Application
 #'
-#' @param ... arguments to pass to golem_opts. 
+#' @param ... arguments to pass to golem_opts.
 #' See `?golem::get_golem_options` for more details.
 #' @inheritParams shiny::shinyApp
 #'
 #' @export
-#' @importFrom shiny shinyApp
-#' @importFrom golem with_golem_options 
+#' @import shiny
+#' @import golem
 run_app <- function(
   onStart = NULL,
-  options = list(), 
+  options = list(),
   enableBookmarking = NULL,
   uiPattern = "/",
   ...
 ) {
-  with_golem_options(
-    app = shinyApp(
+  golem::with_golem_options(
+    app = shiny::shinyApp(
       ui = app_ui,
       server = app_server,
       onStart = onStart,
-      options = options, 
-      enableBookmarking = enableBookmarking, 
+      options = options,
+      enableBookmarking = enableBookmarking,
       uiPattern = uiPattern
-    ), 
+    ),
     golem_opts = list(...)
   )
 }
